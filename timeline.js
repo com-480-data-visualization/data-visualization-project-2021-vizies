3b03a61f2092b709730a47e93a4b8c65616b89fc
Alexander Olsson <aleol711@student.liu.se>
2021-06-01 13:42:19 +0200

    added comments to timeline

--- timeline.js
+++ timeline.js
@@ -1,4 +1,6 @@
-
+/*
+* Class for the timeline with a dragable brush to choose which data that is displayed. 
+*/
 class Timeline {
 
     /* ===== Constants ===== */
@@ -14,7 +16,7 @@ class Timeline {
         const dates = energy_consumption.map(d => d.Date);
         const minDate = new Date(d3.min(dates).setDate(d3.min(dates).getDate() -1));
         const maxDate = new Date(d3.max(dates).setDate(d3.max(dates).getDate() -1)); 
-
+        
         const svg = d3.select('div#'+svg_element_id)
                        .append('svg')
                        .attr("viewBox", [this.margin.left, this.margin.top, this.width-this.margin.right, this.height-this.margin.bottom]);
@@ -23,6 +25,7 @@ class Timeline {
                     .domain([minDate, maxDate])
                     .rangeRound([this.margin.left, this.width-this.margin.right]);
 
+        // An axis that represents the labels over each year, set at 18/6 to be in middle of year
         const yearLabels = d3.axisTop(x)
                              .tickSize(0)
                              .ticks(d3.timeDay)
@@ -35,22 +38,26 @@ class Timeline {
                                  }
                              });
 
+        // An axis that is the line between every year.
         const yearLines = d3.axisBottom(x)
                             .tickSize(2)
                             .ticks(d3.timeYear)
                             .tickFormat((d) => {return null;})
 
+        // Axis that writes the month as labels
         const monthLabels = d3.axisTop(x)
                               .tickSize(0)
                               .ticks(d3.timeMonth)
                               .tickFormat(d3.timeFormat("%b"));
-
+        
+        // Brush that is over the month labels
         const brush = d3.brushX()
                         .extent([[brushPlacement.left, brushPlacement.top], [brushPlacement.right, brushPlacement.bottom]])
                         .on("start", brushed)
                         .on("brush", brushed)
                         .on("end", brushEnd);
 
+        // called on brush start and mid brush to discritize the brush
         function brushed(event) {
             if (!event.sourceEvent) { return; } 
             if (!event.selection) {return; }
@@ -65,10 +72,9 @@ class Timeline {
         
             d3.select(this).call(brush.move, d1.map(x));
             placeHadles();
-
-            // Send d1 to Map and Plot
         }
 
+        // Called on end of brush to send the selected dates
         function brushEnd(event) {
             if (!event.selection) {return; }
 
@@ -78,6 +84,7 @@ class Timeline {
             sendDates(d1);
         }
 
+        // Transforms to correct dates based on padding and sends it to main
         function sendDates(d1) {
             const startDatePadding = -3;
             const endDatePadding = -3;
@@ -87,12 +94,14 @@ class Timeline {
             main.UpdateData(startDate, endDate);
         }
 
+        // Place handles correctly to be over month labels.
         function placeHadles() {
             d3.selectAll(".handle").attr("y", 15.5);
             var x = parseInt(d3.select(".handle--w").attr("x"));
             d3.select(".handle--w").attr("x", x+3);
         }
 
+        // Draw out all axises and brushes.
         this.drawYears(svg, yearLabels);
         this.drawYearsLine(svg, yearLines);
         this.drawMonths(svg, monthLabels);
